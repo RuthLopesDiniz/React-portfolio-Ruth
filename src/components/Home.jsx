173a10a6227b6773edb49f814f046db250cbeca6
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-07-29 16:36:14 -0300

    Start project portifolio: menu responsive ok.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='px-7 md:px-10 my-14 md:h-screen bg-[#1a1a29]'>
+            <div>
+                <div>
+                    {/*Texts */}
+                    <div className='text-white'>
+                        <h6>Oi, eu sou</h6>
+                        <h1>Ruth Diniz</h1>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
