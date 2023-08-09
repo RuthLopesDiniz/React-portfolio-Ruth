1caa2b238539ecdcc04e83262153a28915db075b
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-08-09 13:28:53 -0300

    Feat: formulário de contato adicionado.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
+import facebook from '../assets/socials/facebook.png'
+import instagram from '../assets/socials/instagram.png'
+import linkedin from '../assets/socials/linkedin.png'
+
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className='px-7 md:px-10 my-10' id='contact'>
+      <div className='flex flex-col md:flex-row'>
+        {/*Text and icons */}
+        <div className='md:w-1/2'>
+          <h1 className='text-3xl mt-16 text-primary font-semibold'>
+            Conecte-se comigo:
+          </h1>
+          <p className='text-white my-2 md:w-2/3 leading-[2]'>
+            Veja as formas!
+          </p>
+
+          {/*Social icons */}
+          <div className='flex'>
+            <a href='' className='ml-8'><img src={linkedin} alt=''/></a>
+            <a href='' className='ml-8'><img src={facebook} alt=''/></a>
+            <a href='' className='ml-8'><img src={instagram} alt=''/></a>
+            
+
+          </div>
+        </div>
+
+        {/* form*/}
+        <div className='md:w-1/2'>
+            <p className='mt-16 text-white text-2xl mb-6'>Contacte-me! vamos trabalhar juntos!</p>
+
+            <form className='space-y-4'>
+              <input type='text' name='name' placeholder='Nome: ' className='bg-[#f5f5f5]
+              bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded'/>
+
+              <input type='text' name='name' placeholder='Email: ' className='bg-[#f5f5f5]
+              bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded'/>
+
+              <textarea name='message' id='message' placeholder='Mensagem' className='bg-[#f5f5f5]
+              bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded'></textarea>
+
+              <button className='btn bg-primary py-2 px-12 text-white hover:bg-white hover:text-primary block
+              transition-all duration-500 '>Enviar</button>
+            </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
 const Projects = () => {
+  const projects=[
+      {
+        title:"Twiter",
+        description: "a live geolocalization",
+        photo: "/src/assets/projects/img1.png"
+      },
+      {
+        title:"Events",
+        description: "a live geolocalization",
+        photo: "/src/assets/projects/img2.png"
+      },
+      {
+        title:"Netflix",
+        description: "a live geolocalization",
+        photo: "/src/assets/projects/img3.png"
+      },
+      
+  ]
   return (
-    <div>
-      projects
+    <div className='px-7 md:px-10 my-8' id="projects">
+      <h1 className='text-3xl mt-5 text-primary font-semibold'>Feature projects</h1>
+      <p className='text-white my-2 md:w-2/3 leading-[2]'>Projetos realizados</p>
+
+      {/*Feature projects */}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 my-6 items-center justify-center'>
+          {
+            projects.map((project, index)=> <div key={index} className='flex flex-col shadow-sm md:w-[343px]'>
+              <a href={project.photo} className='mb-4'>
+                <img src={project.photo} alt="" />
+              </a>
+              <h3 className='text-primary font-semibold text-lg'>{project.title}</h3>
+              <p className='text-white mt-1'>{project.description}</p>
+
+              {/*Buttons */}
+              <div className='mt-5'>
+                            <button className='btn bg-primary py-2 px-4 text-white rounded hover:bg-white hover:text-primary transition-all
+                            duration-500'>Sobre mim</button>
+                            <button className='btn outline px-9 py-1.5 rounded border-none text-white ml-5'>Projetos</button>
+                        </div>
+            </div>)
+          }
+      </div>
     </div>
   );
 };
